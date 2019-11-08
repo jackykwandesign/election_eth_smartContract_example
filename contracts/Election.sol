57ef8db12eac41c57c8898b0982d5129547f0c2c
JackyKwan <jackykwandesign@gmail.com>
2019-11-08 17:49:08 +0800

    test

---
+++ contracts/Election.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.11;
+
+contract Election{
+    // store candidate
+    // Read candidate
+    // constructor
+    string public candidate;
+
+    constructor() public {
+        candidate = "Candidate 1";
+    }
+}
